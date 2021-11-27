e34fcf3f3ddefc8263c4a4b9deb17293a6e62057
Pavel Borisov <pavellbor@gmail.com>
2021-11-27 19:44:12 +0300

    Исправляет ошибки линтера, рефакторит код

--- src/main.js
+++ src/main.js
@@ -1,32 +1,29 @@
-import { createUserRankTemplate } from "./view/user-rank-view.js";
-import { createSiteMenuTemplate } from "./view/site-menu-view.js";
-import { createSiteSortTemplate } from "./view/site-sort-view.js";
-import { createFilmListTemplate } from "./view/film-list-view.js";
-import { createFilmCardTemplate } from "./view/film-card-view.js";
-import { createShowMoreTemplate } from "./view/show-more-view.js";
-import { createFilmTotalCountTemplate } from "./view/film-total-count-view.js";
+import { createUserRankTemplate } from './view/user-rank-view.js';
+import { createSiteMenuTemplate } from './view/site-menu-view.js';
+import { createSiteSortTemplate } from './view/site-sort-view.js';
+import { createFilmListTemplate } from './view/film-list-view.js';
+import { createFilmCardTemplate } from './view/film-card-view.js';
+import { createShowMoreButtonTemplate } from './view/show-more-button-view.js';
+import { createFilmTotalCountTemplate } from './view/film-total-count-view.js';
+import { renderTemplate, renderPosition } from './utils.js';
 
-const renderTemplate = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
-};
+const CARD_COUNT = 5;
 
-const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = document.querySelector('.header');
-const footerStatisticsElement = document.querySelector('.footer__statistics');
+const siteMainElement = document.querySelector('.main');
+const siteFooterStatisticsElement = document.querySelector('.footer__statistics');
 
-const CARD_COUNT = 5
-
-renderTemplate(siteHeaderElement, createUserRankTemplate(), 'beforeend');
-renderTemplate(siteMainElement, createSiteMenuTemplate(), 'beforeend');
-renderTemplate(siteMainElement, createSiteSortTemplate(), 'beforeend');
-renderTemplate(siteMainElement, createFilmListTemplate(), 'beforeend');
+renderTemplate(siteHeaderElement, createUserRankTemplate(), renderPosition.BEFOREEND);
+renderTemplate(siteMainElement, createSiteMenuTemplate(), renderPosition.BEFOREEND);
+renderTemplate(siteMainElement, createSiteSortTemplate(), renderPosition.BEFOREEND);
+renderTemplate(siteMainElement, createFilmListTemplate(), renderPosition.BEFOREEND);
 
 const filmsListElement = siteMainElement.querySelector('.films-list');
 const filmsListContainerElement = filmsListElement.querySelector('.films-list__container');
 
 for (let i = 0; i < CARD_COUNT; i++) {
-  renderTemplate(filmsListContainerElement, createFilmCardTemplate(), 'beforeend');
+  renderTemplate(filmsListContainerElement, createFilmCardTemplate(), renderPosition.BEFOREEND);
 }
 
-renderTemplate(filmsListElement, createShowMoreTemplate(), 'beforeend');
-renderTemplate(footerStatisticsElement, createFilmTotalCountTemplate(), 'beforeend');
+renderTemplate(filmsListElement, createShowMoreButtonTemplate(), renderPosition.BEFOREEND);
+renderTemplate(siteFooterStatisticsElement, createFilmTotalCountTemplate(), renderPosition.BEFOREEND);
--- src/view/film-total-count-view.js
+++ src/view/film-total-count-view.js
@@ -1,3 +1,3 @@
 export const createFilmTotalCountTemplate = () => (
-  `<p>130 291 movies inside</p>`
+  '<p>130 291 movies inside</p>'
 );
