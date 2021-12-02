47a99c287a8fe61a289dd1e702d1bd12100e61e7
Pavel Borisov <pavellbor@gmail.com>
2021-12-02 22:34:41 +0300

    Добавляет вспомогательные функции: renderElement и createElement

--- src/render.js
+++ src/render.js
@@ -8,3 +8,27 @@ export const renderPosition = {
 export const renderTemplate = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
+
+export const renderElement = (container, element, place) => {
+  switch (place) {
+    case renderPosition.BEFOREBEGIN:
+      container.before(element);
+      break;
+    case renderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case renderPosition.BEFOREEND:
+      container.append(element);
+      break;
+    case renderPosition.AFTEREND:
+      container.after(element);
+      break;
+  }
+};
+
+export const createElement = (template) => {
+  const newElement = document.createElement('div');
+  newElement.innerHTML = template;
+
+  return newElement.firstElementChild;
+};
